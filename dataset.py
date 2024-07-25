2389c0dcf6acf8891c74c1cb3f14c99e0936fb7b
Gabriel Sperrer <gabriel.sperrer@gmail.com>
2024-07-25 10:17:09 +0200

    final implementation

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import torch
+import torchtext
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from sklearn.preprocessing import LabelEncoder
@@ -8,6 +9,8 @@ from nltk.corpus import reuters
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import nltk
+
+torchtext.disable_torchtext_deprecation_warning()
 
 
 class ReutersDataset(Dataset):
@@ -19,9 +22,8 @@ class ReutersDataset(Dataset):
         return len(self.labels)
 
     def __getitem__(self, idx: int):
-        # This operation copies the data and moves it to the GPU
+        # This operation copies the data
         # Probably we could optimize this quite radically
-        print("Copying data")
         text = torch.tensor(self.texts[idx], dtype=torch.long)
         label = torch.tensor(self.labels[idx], dtype=torch.long)
 
@@ -65,22 +67,28 @@ def load_dataset():
     return encoded_df, vocab, label_encoder
 
 
-def to_dataloader(text: pd.Series, labels: pd.Series, vocab: Vocab, shuffle: bool = True) -> DataLoader:
+def to_dataloader(
+    text: pd.Series,
+    labels: pd.Series,
+    vocab: Vocab,
+    shuffle: bool = True,
+) -> DataLoader:
     dataset = ReutersDataset(text.tolist(), labels.tolist())
     return DataLoader(
         dataset,
         batch_size=32,
         shuffle=shuffle,
         collate_fn=lambda x: _collate_fn(x, vocab),
-        generator=torch.Generator(device=torch.device("mps"))
     )
 
 
-def _collate_fn(batch: list[tuple[torch.Tensor, torch.Tensor]], vocab: Vocab):
+def _collate_fn(batch: list[tuple[list[int], list[int]]], vocab: Vocab):
     texts, labels = zip(*batch)
+
     texts_padded = pad_sequence(texts, batch_first=True, padding_value=vocab["<pad>"])
-    print("Copying data")
-    return texts_padded, torch.tensor(labels)
+    labels = torch.tensor(labels, dtype=torch.long)
+
+    return texts_padded, labels
 
 
 def _remove_stopwords(text: list[str]) -> pd.Series:
@@ -89,9 +97,7 @@ def _remove_stopwords(text: list[str]) -> pd.Series:
         pd.Series(text)
         .str.lower()
         .replace("[^\w\s]", "")
-        .apply(
-            lambda x: " ".join([word for word in x.split() if word not in stop]),
-        )
+        .apply(lambda x: " ".join([word for word in x.split() if word not in stop]))
     )
 
 
